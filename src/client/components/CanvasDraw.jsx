44e2700d7e114557dd1ce5954a3736dc3b212119
Idan.Ofek <edanofek@gmail.com>
2018-06-16 20:33:21 +0300

    Add ablity to draw on canvas

--- src/client/components/CanvasDraw.jsx
+++ src/client/components/CanvasDraw.jsx
@@ -23,14 +23,60 @@ class CanvasDraw extends Component {
       context.drawImage(imageObj, 0, 0);
     };
     imageObj.src = 'http://localhost:'+processNumber+'/image/'+this.props.id; 
+
+    let clickX = new Array();
+    let clickY = new Array();
+    let clickDrag = new Array();
+    let paint;
  
+    canvas.addEventListener("mousedown",function(e){
+      var mouseX = e.pageX - this.offsetLeft;
+      var mouseY = e.pageY - this.offsetTop;
+        
+      paint = true;
+      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+      redraw();
+    });
     canvas.addEventListener("mousemove",function(e){
-      //TODO:Con't here
       if(paint){
         addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
         redraw();
       }
     });
+    canvas.addEventListener("mousedown",function(e){
+      paint = false;
+    });
+    canvas.addEventListener("mouseleave",function(e){
+      paint = false;
+    });
+
+    function addClick(x, y, dragging)
+    {
+      clickX.push(x);
+      clickY.push(y);
+      clickDrag.push(dragging);
+    }
+
+    function redraw(){
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+      
+      context.strokeStyle = "#df4b26";
+      context.lineJoin = "round";
+      context.lineWidth = 5;
+          
+      for(var i=0; i < clickX.length; i++) {		
+        context.beginPath();
+        if(clickDrag[i] && i){
+          context.moveTo(clickX[i-1], clickY[i-1]);
+         }else{
+           context.moveTo(clickX[i]-1, clickY[i]);
+         }
+         context.lineTo(clickX[i], clickY[i]);
+         context.closePath();
+         context.stroke();
+      }
+    }
+
   }
 
   render() {
