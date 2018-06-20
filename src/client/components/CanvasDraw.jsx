f2fe214982e335d370a497cd651c5673fe790ad6
Idan.Ofek <edanofek@gmail.com>
2018-06-21 00:38:29 +0300

    Add allow drawing state  field

--- src/client/components/CanvasDraw.jsx
+++ src/client/components/CanvasDraw.jsx
@@ -10,7 +10,7 @@ class CanvasDraw extends Component {
 
     super(props);
     this.state = {
-      
+      allowDrawing:false
     };
 
     this.canvasLogic = null;
--- src/client/js/CanvasLogic.js
+++ src/client/js/CanvasLogic.js
@@ -63,6 +63,7 @@ class CanvasLogic {
                 context.stroke();
             }
         }
+        
     }
 
     logicClearCanvas(){
