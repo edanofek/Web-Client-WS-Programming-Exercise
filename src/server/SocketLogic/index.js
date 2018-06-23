e5d1b4ad57742739ef6a8c0f9ef24bf79be3550d
Idan.Ofek <edanofek@gmail.com>
2018-06-24 01:03:14 +0300

    Add module socket logic

--- src/server/SocketLogic/index.js
+++ src/server/SocketLogic/index.js
@@ -1,10 +1,14 @@
 var SocketLogic = function(io){
 
-
     // TODO:Con't here
-    
+
     //each line is holding canvas id,line history
-    // var line_history = [{}];
+    var line_history = [{}];
+
+    
+    for (var i in line_history) {
+        socket.emit('draw_line', { line: line_history[i] } );
+    }
 
 };
 
