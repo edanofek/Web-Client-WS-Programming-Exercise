efa1066d09131b3994cfa2f912c6b0973aa7eb46
Idan.Ofek <edanofek@gmail.com>
2018-07-22 12:07:07 +0300

    Add support to css-grid

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -4,30 +4,25 @@ import CanvasDraw from './CanvasDraw.jsx';
 
 class App extends Component {
   render() {
-    return <div className="container">
-        <div className="row">
-          <div className="col3rd">
-            <CanvasDraw id={1}/>
-          </div>
-          <div className="col3rd">
-            <CanvasDraw id={2}/>
-          </div>
-          <div className="col3rd">
-            <CanvasDraw id={3}/>
-          </div>
-        </div>
-        <div/>
-        <div className="row">
-          <div className="col3rd">
-            <CanvasDraw id={4}/>
-          </div>
-          <div className="col3rd">
-            <CanvasDraw id={5}/>
-          </div>
-          <div className="col3rd">
-            <CanvasDraw id={6}/>
-          </div>
-        </div>
+    return <div className="wrapper">
+            <div class="box">
+              <CanvasDraw id={1}/>
+            </div>
+            <div class="box">
+              <CanvasDraw id={2}/>
+            </div>
+            <div class="box">
+              <CanvasDraw id={3}/>
+            </div>
+            <div class="box">
+              <CanvasDraw id={4}/>
+            </div>
+            <div class="box">
+              <CanvasDraw id={5}/>
+            </div>
+            <div class="box">
+              <CanvasDraw id={6}/> 
+            </div>
       </div>
     
   }
